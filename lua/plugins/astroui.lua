244aee9dd8871a6b05cb02c4b9f476fae53f4fa9
yut19216812 <yut19216811@gmail.com>
2024-06-02 22:52:26 +0900

    updated

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -12,10 +12,11 @@ return {
   opts = {
     -- change colorscheme
     -- colorscheme = "astrodark",
-    colorscheme = "tokyonight-night",
+    -- colorscheme = "tokyonight-night",
     -- colorscheme = "carbonfox",
-    -- colorscheme = "nord",
+    -- colorscheme = "duskfox",
     -- colorscheme = "poimandres",
+    colorscheme = "rose-pine",
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
     highlights = {
       init = { -- this table overrides highlights in all themes
